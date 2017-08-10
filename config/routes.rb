4617ad6a64958e6bc3ea56d6304d3f7e9f387548
Faye Hayes <fhayes30@gmail.com>
2017-08-09 18:09:08 -0700

    complete routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :proposals do
+    resources :experiments
+  end
+
+  resources :experiments do
+    resources :procedures
+  end
+  resources :users
+
+  get '/login' => 'sessions#new'
+
+  root 'proposals#index'
 end
