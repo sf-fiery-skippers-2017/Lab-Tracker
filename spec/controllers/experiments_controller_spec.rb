f3db13236e94762bfd4f242571281d367f259332
Faye Hayes <fhayes30@gmail.com>
2017-08-10 15:53:38 -0700

    start on spec file. come back to it. Throwing odd error.

--- spec/controllers/experiments_controller_spec.rb
+++ spec/controllers/experiments_controller_spec.rb
@@ -2,4 +2,13 @@ require 'rails_helper'
 
 RSpec.describe ExperimentsController, type: :controller do
 
+  # let!(:proposal)   { Proposal.first }
+  # let!(:experiment) { proposal.experiments }
+  # let!(:comments)   { proposal.comments }
+  #
+  # describe "GET # INDEX" do
+  #   it "responds with status code 200" do
+  #     expect(response)to have_http_status 200bu
+  #   end
+  # end
 end
