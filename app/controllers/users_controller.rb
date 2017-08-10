1993d10ab001a93102c6a2e662f8048dfd30c06d
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-10 15:34:19 -0700

    UPDATE UsersController new and create methods
    add the new and create methods to UsersController for user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    redirect_to '/'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :credentials, :email, :password)
+  end
 end
