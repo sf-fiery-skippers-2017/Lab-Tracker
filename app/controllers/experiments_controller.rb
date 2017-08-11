816a6900f45106d3a6cea9f9c474ed03f4bb964a
alphonsotran <alphonsotran@gmail.com>
2017-08-10 20:19:37 -0700

    Add new experiment form
    Add new experiment form with controller and views linked to it.

--- app/controllers/experiments_controller.rb
+++ app/controllers/experiments_controller.rb
@@ -1,7 +1,42 @@
 class ExperimentsController < ApplicationController
+   before_action :set_experiment, only: [:show, :edit, :update, :destroy]
+
   def index
     @proposal = Proposal.find(params[:proposal_id])
     @experiments = @proposal.experiments
     @comments = @proposal.comments
+  end
+
+  def show
+  end
+
+  def new
+    @experiment = Experiment.new
+  end
+
+  def create
+    @proposal = Proposal.find(params[:proposal_id])
+    @experiment = @proposal.experiments.new(exp_params)
+    @experiment.lab_staff_id = session[:user_id]
+    if @experiment.save
+      redirect_to proposal_experiment_path(@proposal, @experiment)
+    else
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def set_experiment
+    @experiment = Experiment.find(params[:id])
+  end
+
+  def exp_params
+    params.require(:experiment).permit(:title)
   end
 end
