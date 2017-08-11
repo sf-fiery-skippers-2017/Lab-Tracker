cb01e38c31e3042ba81b79cd0afd0069c8826fb4
Elsha Eliasa Kwee <elsha.e.k@gmail.com>
2017-08-10 19:36:25 -0700

    define set_user method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,6 +15,10 @@ class UsersController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find(params[:id])
+  end
+
   def user_params
     params.require(:user).permit(:first_name, :last_name, :credentials, :email, :password)
   end
